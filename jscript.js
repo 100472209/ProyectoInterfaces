16a4564e6aa0a76c97deb6f36b8aa336eadca32a
ireneedg <100451952@alumnos.uc3m.es>
2023-12-15 16:21:52 +0100

    cambios

---
+++ jscript.js
@@ -0,0 +1,21 @@
+let indiceCuadradoActual = 0;
+const cuadradosPorPagina = 4;
+
+function cambiarCuadrado(direccion) {
+    const cuadrados = document.querySelectorAll('.cuadrado');
+    const numCuadrados = cuadrados.length;
+
+    const cuadradosVisibles = Math.min(cuadradosPorPagina, numCuadrados);
+    const limite = numCuadrados - cuadradosVisibles;
+
+    indiceCuadradoActual += direccion;
+
+    if (indiceCuadradoActual < 0) {
+        indiceCuadradoActual = 0;
+    } else if (indiceCuadradoActual > limite) {
+        indiceCuadradoActual = limite;
+    }
+
+    const offset = -indiceCuadradoActual * cuadrados[0].offsetWidth;
+    document.querySelector('.galeria-pedido').style.transform = `translateX(${offset}px)`;
+}
