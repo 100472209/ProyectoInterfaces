2ff9aaf5944db60fd649b2ecc69f2ce1b7781d2f
rodrigo <100472209@alumnos.uc3m.es>
2023-12-16 12:09:19 +0100

    ocultar y mostrar secciones

--- jscript.js
+++ jscript.js
@@ -1,4 +1,25 @@
-// Script para la galería de imágenes de las hamburguesas
+document.addEventListener("DOMContentLoaded", function() {
+    // Agregar el evento click a todos los enlaces que afectan a las secciones
+    document.querySelectorAll('nav ul li a, div.fotos a, a.pide, div.user a, form.formulario a').forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            var sectionId = this.getAttribute('href').slice(1);
+
+            // Ocultar todas las secciones
+            document.querySelectorAll('.section').forEach(function(section) {
+                section.style.display = 'none';
+            });
+
+            // Mostrar la sección correspondiente
+            var targetSection = document.getElementById(sectionId);
+            if (targetSection) {
+                targetSection.style.display = 'flex';
+            }
+        });
+    });
+});
+
+
 let indiceCuadradoActual = 0;
 const cuadradosPorPagina = 3;
 
